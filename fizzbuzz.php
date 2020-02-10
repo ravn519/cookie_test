085f3dc3365e0b9b5694a785d05a2e18b649d55a
Ryan van Niekerk <ryan@pdmartech.com>
2020-02-10 17:18:18 -0600

    fizzbuzz edit

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,26 +1,31 @@
 <?php
 
-for ($end = 1; $end <= 100; $end++) {
+function fizzbuzz($end = 100){
 
-	if($end % 3 == 0 && $end % 5== 0) {
+for ($i = 1; $i <=$end; $i++) {
+
+	if($i % 3 == 0 && $i % 5== 0) {
 		echo "FizzBuzz";
 		echo "\n";
 		}
 
-	elseif($end % 3 == 0) {
+	elseif($i % 3 == 0) {
 		echo "Fizz";
 		echo "\n";
 		}
 
-	elseif($end % 5 == 0) {
+	elseif($i % 5 == 0) {
 		echo "Buzz";
 		echo "\n";
 		}
 
 	else{
-		echo "$end";
+		echo "$i";
 		echo "\n";
 	}
 }
+}
+
+fizzbuzz($end = 100);
 
 ?>
