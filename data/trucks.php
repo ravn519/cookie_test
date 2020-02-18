5ad712b3770333f46d27510cfd7a8f4b2dc06f71
Nick <nick@pdmartech.com>
2020-02-18 13:36:05 -0600

    Added Trucks Sample Data

---
+++ data/trucks.php
@@ -0,0 +1,24 @@
+<?php
+
+    $models = [
+        "FE180 / FEC92S",
+        "FG4X4 / FGB72S",
+        "FE130 / FEC52S",
+        "FE160 / FEC72S",
+        "FE160 C-CAB / FEC72W",
+        "FE140 Gas/FECZTS",
+        "FE160 Gas/FEC7TS",
+        "FE160 Gas C-CAB/FEC7TW",
+        "FE180 Gas/FEC9TS"
+    ];
+
+    $wheelbases = [
+        "151.6 G",
+        "169.3 H",
+        "187.0 K",
+        "134.4 E",
+        "110.2 C",
+        "133.9 E"
+    ];
+
+?>
