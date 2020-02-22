c9a71aff08cedeb650e1d4e3692b2954cd4210e8
Ryan van Niekerk <ryan@pdmartech.com>
2020-02-22 15:52:28 -0600

    date and time utility functions #10.5.5

--- lib/utility.php
+++ lib/utility.php
@@ -4,12 +4,50 @@ class Utility {
 
     static function list($arr){
 
-        for ($a = 0; $a < count($arr) ; $a++){ 
+        foreach ($arr as $value) {
 
-            echo $arr[$a];
+            echo $value;
             echo "\n";
 
-        } 
+        }
+    }
+
+    static function getDate($mode = NULL){
+
+        switch ($mode) {
+            case "short":
+                return date("m/s/Y");
+                break;
+
+            case "long":
+                return date("F dS, Y");
+                break;
+            
+            default:
+                return date("M dS, Y");
+                break;
+        }
+
+    }
+
+    static function getTime($mode = NULL){
+
+        date_default_timezone_set("America/Chicago");
+
+        switch ($mode) {
+            case "short":
+                return date("g:i a");
+                break;
+
+            case "long":
+                return date("h:i:s A");
+                break;
+            
+            default:
+                return date("G:i");
+                break;
+        }
+
 
     }
 
